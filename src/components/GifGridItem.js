41bfac7721700790802c5108422dbbfdf3331f79
xXpilintuXx <ema-nuel06@hotmail.com>
2021-01-20 19:24:44 -0600

    Agregando compatibilidad con vistas de celular

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -9,13 +9,15 @@ export const GifGridItem = ( { id, title, url} ) => {
     return (
         <>
         
-        <div className="col s2 m2">
+        <div className="col s6 m3">
         <div className="card">
             <div className="card-image">
             <img src= {
                 url
+            }
+            alt={
+                title
             }></img>
-            <span className="card-title">{title}</span>
             </div>
           </div>
           </div>
